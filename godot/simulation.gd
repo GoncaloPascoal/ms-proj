5b5f72c87580f40016ad060db2e8c6502a68ed9a
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-12-23 17:00:28 +0000

    Implement TCP sockets in Godot visualization component

--- godot/simulation.gd
+++ godot/simulation.gd
@@ -3,36 +3,29 @@ extends Spatial
 const EARTH_RADIUS := 6.371e6
 const SCALE := 2e-6
 
-const FPS = 20.0
-
 export(PackedScene) var satellite_scene
 
 onready var earth: MeshInstance = $Earth
 onready var satellites_root: Spatial = $SatellitesRoot
 
-var time_since_start = 0
-var timestamp_index = 1
-var sim_data = {}
+var _status := 0
+var _stream := StreamPeerTCP.new()
 
 func _ready():
 	var mesh = SphereMesh.new()
 	mesh.radius = EARTH_RADIUS * SCALE
 	mesh.height = 2 * mesh.radius
 	mesh.material = load("mat.tres")
-
+	
 	earth.mesh = mesh
-
-	var file = File.new()
-	var filename = "../data/test.sim"
-	if not file.file_exists(filename):
-		print("File not found")
+	
+	# Socket connection
+	if _stream.connect_to_host("127.0.0.1", 1234) != OK:
+		print("Error connecting to server.")
 		return
-	file.open(filename, File.READ)
-	sim_data = parse_json(file.get_as_text())
-	_init_simulation(sim_data[0])
 
-func list_to_vector3(l) -> Vector3:
-	return Vector3(l[0], l[1], l[2])
+func array_to_vector3(arr: Array) -> Vector3:
+	return Vector3(arr[0], arr[1], arr[2])
 
 func _init_simulation(json):
 	var satellites: Dictionary = json["satellites"]
@@ -48,20 +41,27 @@ func _update_simulation(json):
 		var data = satellites[id]
 		var satellite = satellites_root.get_child(int(id))
 		
-		var position = list_to_vector3(data["position"]) * SCALE
-		var velocity = list_to_vector3(data["velocity"]) * SCALE
+		var position = array_to_vector3(data["position"]) * SCALE
+		var velocity = array_to_vector3(data["velocity"]) * SCALE
 
 		satellite.reset_physics_interpolation()
 		satellite.global_translation = position
 		satellite.velocity = velocity
 
 func _physics_process(_delta: float):
-	# ativar o update_simulation conforme necessário
-	time_since_start += _delta
-	if timestamp_index < len(sim_data) and timestamp_index / FPS < time_since_start:
-		_update_simulation(sim_data[timestamp_index])
-		timestamp_index += 1
+	_status = _stream.get_status()
 
-func _input(event: InputEvent):
-	if event is InputEventMouseMotion:
-		pass
+	if _status == _stream.STATUS_CONNECTED:
+		var bytes := _stream.get_available_bytes()
+		if bytes > 0:
+			var data := _stream.get_partial_data(bytes)
+			if data[0] == OK:
+				var json = parse_json(data[1].get_string_from_utf8())
+				_handle_msg(json)
+
+func _handle_msg(json):
+	match json["msg_type"]:
+		"init":
+			_init_simulation(json)
+		"update":
+			_update_simulation(json)

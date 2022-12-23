2dcf94b4f556a2b590866d7f95d11a6c2475a669
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-12-23 18:00:22 +0000

    Return to using WebSockets

--- godot/simulation.gd
+++ godot/simulation.gd
@@ -3,26 +3,28 @@ extends Spatial
 const EARTH_RADIUS := 6.371e6
 const SCALE := 2e-6
 
+export(String) var websocket_url = "ws://localhost:1234"
 export(PackedScene) var satellite_scene
 
 onready var earth: MeshInstance = $Earth
 onready var satellites_root: Spatial = $SatellitesRoot
 
-var _status := 0
-var _stream := StreamPeerTCP.new()
+var _client := WebSocketClient.new()
 
 func _ready():
 	var mesh = SphereMesh.new()
 	mesh.radius = EARTH_RADIUS * SCALE
 	mesh.height = 2 * mesh.radius
-	mesh.material = load("mat.tres")
-	
+
 	earth.mesh = mesh
 	
-	# Socket connection
-	if _stream.connect_to_host("127.0.0.1", 1234) != OK:
-		print("Error connecting to server.")
-		return
+	_client.connect("connection_established", self, "_connected")
+	_client.connect("data_received", self, "_on_data")
+
+	var err := _client.connect_to_url(websocket_url)
+	if err != OK:
+		print("Unable to connect to host.")
+		set_physics_process(false)
 
 func array_to_vector3(arr: Array) -> Vector3:
 	return Vector3(arr[0], arr[1], arr[2])
@@ -49,19 +51,16 @@ func _update_simulation(json):
 		satellite.velocity = velocity
 
 func _physics_process(_delta: float):
-	_status = _stream.get_status()
+	_client.poll()
 
-	if _status == _stream.STATUS_CONNECTED:
-		var bytes := _stream.get_available_bytes()
-		if bytes > 0:
-			var data := _stream.get_partial_data(bytes)
-			if data[0] == OK:
-				var json = parse_json(data[1].get_string_from_utf8())
-				_handle_msg(json)
+func _connected(_proto: String = ""):
+	print("Connected to host!")
 
-func _handle_msg(json):
-	match json["msg_type"]:
-		"init":
-			_init_simulation(json)
-		"update":
-			_update_simulation(json)
+func _on_data():
+	var json := JSON.parse(_client.get_peer(1).get_packet().get_string_from_utf8())
+
+	if json.error == OK:
+		var result = json.result
+		match result["msg_type"]:
+			"init": _init_simulation(result)
+			"update": _update_simulation(result)

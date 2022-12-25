c1e117aabf89b189c0af7e3535d135158b1c3112
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-12-25 20:08:46 +0000

    Highlight selected satellite and show its orbital plane

--- godot/satellite.gd
+++ godot/satellite.gd
@@ -2,7 +2,18 @@ extends KinematicBody
 
 export(int) var id := 0
 
+const LIGHT_ENERGY := 10.0
+
+onready var _light: OmniLight = $Light
+
+var orbital_plane: Dictionary
 var velocity := Vector3.ZERO
 
 func _physics_process(_delta: float):
 	velocity = move_and_slide(velocity)
+
+func enable_light():
+	_light.light_energy = LIGHT_ENERGY
+
+func disable_light():
+	_light.light_energy = 0.0
--- godot/simulation.gd
+++ godot/simulation.gd
@@ -4,6 +4,7 @@ signal satellite_selected(satellite)
 
 const EARTH_RADIUS := 6.371e6
 const SCALE := 2e-6
+const MAX_RAY_LENGTH := 1000.0
 
 export(String) var websocket_url = "ws://localhost:1234"
 export(PackedScene) var satellite_scene
@@ -11,8 +12,13 @@ export(PackedScene) var satellite_scene
 onready var hud: Control = $HUD
 onready var satellites_root: Spatial = $SatellitesRoot
 onready var camera: Camera = $CameraGimbal/InnerGimbal/Camera
+onready var orbital_plane: MeshInstance = $OrbitalPlane
+
+var _orbital_planes: Dictionary
+var _inclination: float
 
 var _client := WebSocketClient.new()
+
 var _selected_satellite: KinematicBody
 
 func _ready():
@@ -33,10 +39,23 @@ func array_to_vector3(arr: Array) -> Vector3:
 
 func _init_simulation(json):
 	var satellites: Dictionary = json["satellites"]
+	var semimajor_axis: float = json["semimajor_axis"]
+	
+	_orbital_planes = json["orbital_planes"]
+	_inclination = json["inclination"]
+	
+	var r = semimajor_axis * SCALE
+	orbital_plane.mesh.top_radius = r
+	orbital_plane.mesh.bottom_radius = r
+	orbital_plane.mesh.height = r * 0.005
+	orbital_plane.rotation.x = _inclination
 	
 	for id in satellites:
+		var data = satellites[id]
 		var instance = satellite_scene.instance()
 		instance.id = int(id)
+		instance.orbital_plane = _orbital_planes[data["orbital_plane"]]
+		
 		satellites_root.add_child(instance)
 	
 	hud.init_hud(json)
@@ -62,15 +81,28 @@ func _physics_process(_delta: float):
 	if Input.is_action_just_pressed("select"):
 		var mouse_pos := get_viewport().get_mouse_position()
 		var from := camera.project_ray_origin(mouse_pos)
-		var to := camera.project_ray_normal(mouse_pos) * camera.far
+		var to := camera.project_ray_normal(mouse_pos) * MAX_RAY_LENGTH
 		
 		var ray_result := get_world().direct_space_state.intersect_ray(from, to, [self])
 		
 		if ray_result:
-			_selected_satellite = ray_result.collider
-			emit_signal("satellite_selected", _selected_satellite)
+			_select_satellite(ray_result.collider)
 	
 	_client.poll()
+
+func _select_satellite(satellite: KinematicBody):
+	if _selected_satellite:
+		_selected_satellite.disable_light()
+	
+	_selected_satellite = satellite
+	if _selected_satellite:
+		_selected_satellite.enable_light()
+		var longitude: float = _selected_satellite.orbital_plane["longitude"]
+		orbital_plane.rotation.y = longitude
+		orbital_plane.visible = true
+		orbital_plane.reset_physics_interpolation()
+	
+	emit_signal("satellite_selected", _selected_satellite)
 
 func _connected(_proto: String = ""):
 	print("Connected to host!")

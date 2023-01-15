dc9f1a69a4a0f2e1f59adb5adcd2d8e317f836d1
Clara Martins <clara.al.martins@gmail.com>
2023-01-15 11:31:06 +0000

    Better Labels on Graphs

--- python/statistics.py
+++ python/statistics.py
@@ -17,27 +17,33 @@ class PlotType(Enum):
 possible_plots = {
     PlotType.DENSITY: {
         'title': 'Density of the Satellite Graph',
-        'y_values': {'graph_density': 'Density'},
+        'y_values': {'graph_density': None},
+        'y_label': 'Density',
     },
     PlotType.CONNECTIVITY: {
         'title': 'Graph Connectivity',
         'y_values': {'connected_components': 'Connected Components', 'articulation_points': 'Articulation Points'},
+        'y_label': None,
     },
     PlotType.CONNECTIONS: {
         'title': 'Active Connections between Satellites',
         'y_values': {'active_connections': 'Active Connections'},
+        'y_label': 'Active Connections (units)',
     },
     PlotType.FAILURES: {
         'title': 'Evolution of Satellite Failures',
-        'y_values': {'failed_satellites': 'Satellite Failures'},
+        'y_values': {'failed_satellites': None},
+        'y_label': 'Satellite Failures (%)',
     },
     PlotType.RTT: {
         'title': 'Round Trip Time (from London to Other Cities)',
         'y_values': {'rtt_nyc': 'New York', 'rtt_singapore' : 'Singapore', 'rtt_johannesburg': 'Johannesburg'},
+        'y_label': 'Round Trip Time (s)',
     },
     PlotType.LATENCY_DISTANCE_RATIO: {
         'title': 'Latency to Distance Ratio (from London to Other Cities)',
         'y_values': {'latency_nyc': 'New York', 'latency_singapore': 'Singapore', 'latency_johannesburg': 'Johannesburg'},
+        'y_label': 'Latency to Distance Ratio (s/m)',
     },
 }
 
@@ -68,14 +74,23 @@ def plot_line(ax: Axes, x: str, y: str, ylabel: str) -> None:
 
 def plot(ax: Axes, p: PlotType) -> None:
     plot = possible_plots[p]
-    y_values = plot['y_values']
     title = plot['title']
+    y_label = plot['y_label']
+    y_values = plot['y_values']
 
     ax.clear()
     ax.set_title(title)
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel(y_label)
+
+    legend = True
     for y, ylabel in y_values.items():
         plot_line(ax, 't', y, ylabel)
-    ax.legend()
+        if ylabel == None:
+            legend = False
+    
+    if legend:
+        ax.legend()
 
 if __name__ == '__main__':
     statistics_figure(values)
